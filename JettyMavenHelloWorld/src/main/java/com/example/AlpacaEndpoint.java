f39bdf3da8d70a233358568666d5973313fb749a
Hiya <hpmaav@gmail.com>
2025-08-05 17:35:16 -0400

    Set Up Listener Class to Alpaca Market Data API

---
+++ JettyMavenHelloWorld/src/main/java/com/example/AlpacaEndpoint.java
@@ -0,0 +1,80 @@
+package com.example;
+
+import org.eclipse.jetty.websocket.api.Callback;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@WebSocket(autoDemand = true)
+public class AlpacaEndpoint implements Session.Listener {
+    private Session session;
+    private static final Logger LOG = LoggerFactory.getLogger(MyEndpoint.class);
+    private ObjectMapper mapper = new ObjectMapper();
+    private static String auth = "{\"action\":\"auth\"," + "\"key\":\"" + System.getenv("ALPACA_API_KEY") + "\",\"secret\":\"" + System.getenv("ALPACA_SECRET") + "\"}";
+    private static String subscribe = "{\"action\":\"subscribe\",\"trades\":[\"FAKEPACA\"],\"quotes\":[\"FAKEPACA\", \"AMD\"]}";    
+          
+    @Override
+    public void onWebSocketOpen(Session session) {
+        // send an authentication and then demand
+        System.out.println("Trying to connect to websocket");
+        this.session = session;
+        session.demand();
+    }
+
+    @Override
+    public void onWebSocketText(String message) {
+        LOG.info("Received data", message);
+        System.out.println(message);
+        try {
+            JsonNode root = mapper.readTree(message);
+            if (root.isArray()) {
+                System.out.println("Array received");
+                for(JsonNode node : root) {
+                    String msg = node.path("msg").asText();
+                    System.out.println(msg);
+                    if(msg.equals("connected")) {
+                        System.out.println(auth);
+                        session.sendText(auth, Callback.from(session :: demand, Throwable :: printStackTrace));
+                    } else if (msg.equals("authenticated")) {
+                        System.out.println("Subscribing");
+                        session.sendText(subscribe, Callback.from(session :: demand, Throwable :: printStackTrace));
+                    } else {
+                        //TO DO: Setup Pipeline from Alpaca Listener to Market Data Serving Endpoint
+                    }
+                }
+            }
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onWebSocketError(Throwable cause)
+    {
+        // The WebSocket endpoint failed.
+
+        // You may log the error.
+        cause.printStackTrace();
+
+    }
+
+    @Override
+    public void onWebSocketClose(int statusCode, String reason)
+    {
+        // The WebSocket endpoint has been closed.
+        System.out.println("Websocket closed");
+    }
+    
+}
--- JettyMavenHelloWorld/src/main/java/com/example/SimplestServer.java
+++ JettyMavenHelloWorld/src/main/java/com/example/SimplestServer.java
@@ -3,6 +3,9 @@ package com.example;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.Connector;
+
+import java.net.URI;
+import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
 import org.eclipse.jetty.ee10.webapp.WebAppContext;
 import org.eclipse.jetty.ee10.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
@@ -13,30 +16,14 @@ import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Response;
 import org.eclipse.jetty.util.Callback;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.client.WebSocketClient;
+import java.util.concurrent.CompletableFuture;
 
 import jakarta.websocket.server.ServerEndpointConfig;
 
 public class SimplestServer
 {
-    /*@Override
-    public boolean handle(Request request, Response response, Callback callback)
-    {
-        if(request.getHttpURI().asString().endsWith("/stocks")) {
-            System.out.println("Stocks requested");
-            response.setStatus(200);
-            response.getHeaders().put(HttpHeader.CONTENT_TYPE, "application/json; charset=UTF-8");
-            Content.Sink.write(response, true, "{stocks: nvidia}", callback);
-        } else {
-            System.out.println("Home Screen Requested");
-            response.setStatus(200);
-            response.getHeaders().put(HttpHeader.CONTENT_TYPE, "text/html; charset=UTF-8");
-            Content.Sink.write(response, true, "<!DOCTYPEHTML><html><body><h1>Dashboard</h1></body></html>", callback);
-        }
-
-        return true;
-    }*/
-
-        //QueuedThreadPool threadPool = new QueuedThreadPool();
     public static Server newServer(int port) 
     {
         Server server = new Server(port);
@@ -53,21 +40,32 @@ public class SimplestServer
 
         });
 
-        // connectors = component that accepts client connections
-        //Connector connector = new ServerConnector(server);
-        //server.addConnector(connector);
         return server;
     }
     
     public static void main(String[] args) {
-        Server server = newServer(8080);
+        HttpClient httpClient = new HttpClient(); 
+        WebSocketClient websocketClient = new WebSocketClient(httpClient); 
+        
         try {
-            server.start();
+            websocketClient.start();
+            AlpacaEndpoint alpacaEndpoint = new AlpacaEndpoint(); 
+            URI serverURI = new URI("wss://stream.data.alpaca.markets/v2/test");
+
+            CompletableFuture<Session> clientSessionPromise = websocketClient.connect(alpacaEndpoint, serverURI);
+
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
-        }
+        } 
+
         
+        /*Server server = newServer(8080);
+        try {
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
     
 }

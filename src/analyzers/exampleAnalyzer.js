48b4ca5f02e31c6184c43ded5c9f527e873f4545
Hiya <hpmaav@gmail.com>
2026-03-03 16:50:10 -0500

    Cleaned up file directory

--- src/analyzers/exampleAnalyzer.js
+++
@@ -1,8 +0,0 @@
-export function exampleAnalyzer(prices) {
-  // Simple moving average stub
-  const symbols = Object.keys(prices);
-  return symbols.map(symbol => ({
-    symbol,
-    insight: `Moving Avg: ${(prices[symbol].reduce((a, b) => a + b, 0) / prices[symbol].length).toFixed(2)}`,
-  }));
-}
